10d56a8dffb06f2e22b442c8a6ee11b908c80021
Hope Ranker <hranker@groupcse.com>
2016-07-06 17:25:08 -0400

    Configure dev pgsql to allow local vagrant

--- provision/config-postgres.sh
+++ provision/config-postgres.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo -u postgres psql template1 -c "create user vagrant superuser password 'vagrant'"
+# Create database user
+sudo -u postgres psql template1 -c "create user vagrant superuser password 'vagrant'" || true
+
+# configure pg_hba.conf for local vagrant access
+hba=/etc/postgresql/9.3/main/pg_hba.conf
+sed -i '/host\s\+all.*127\.0\.0\.1/s/^#//' /etc/postgresql/9.3/main/pg_hba.conf
+sed -i '/host\s\+all.*::1/s/^#//' /etc/postgresql/9.3/main/pg_hba.conf
+service postgresql restart
